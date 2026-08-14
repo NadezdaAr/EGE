f454994e0e372d150e6cbc36979a44431ee94786
Nadezda <arcukevicnadeza@gmail.com>
2026-08-14 17:35:27 +0200

    Преобразование типов данных

---
+++ Base/lesson-3.py
@@ -0,0 +1,8 @@
+# Преобразование типов данных
+# int -> float, str, bool
+# float -> int, str, bool
+# str -> str, float, list, tuple, set, bool
+# list -> str, tuple, set, bool
+# tuple -> str, list, set, bool
+# set -> str, list, tuple, bool
+# bool -> int, float, str
